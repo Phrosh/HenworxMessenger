eeb823c084010e097e8149e8723ae4a3aadec118
Phrosh <phrosh@gmx.net>
2021-05-22 19:51:06 +0200

    Added basic UI elements

---
+++ style.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  height: 100%;
+  flex-direction: column-reverse;
+  padding-bottom: 10px;
+`;
+
+export const TextInputPanel = styled.View`
+  flex-direction: row;
+  margin: 10px;
+  height: 40px;
+  align-items: center;
+`;
